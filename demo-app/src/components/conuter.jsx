2e35601029da0be2113a9a1844607fe20e164218
Mohammed <m7md21999@gmail.com>
2020-05-20 22:36:53 +0300

    rednring lists with if condtions

--- demo-app/src/components/conuter.jsx
+++ demo-app/src/components/conuter.jsx
@@ -11,17 +11,24 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.renderTags()}
+      </div>
+    );
+  }
 
-        {/* //rending lists */}
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>Tags list is emphty</p>;
+    else {
+      /* //rending lists */
+      return (
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
-      </div>
-    );
+      );
+    }
   }
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count == 0 ? "warning" : "primary";
